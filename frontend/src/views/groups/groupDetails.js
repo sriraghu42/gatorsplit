0a1fdf7e5a6dbd36c5d618f694ca39037211b1e1
varunrudrangi <varun.rudrangi@gmail.com>
2025-04-21 17:51:48 -0400

    changing integration of settleup as per modifications on the modal

--- frontend/src/views/groups/groupDetails.js
+++ frontend/src/views/groups/groupDetails.js
@@ -292,6 +292,7 @@ const GroupDetails = ({ groupId, groupName }) => {
                 currentUser={userid}
                 groupId={groupId}
                 fetchExpenses={fetchExpenses}
+                allowGroupSelect={false}
             />
 
         </Box>
